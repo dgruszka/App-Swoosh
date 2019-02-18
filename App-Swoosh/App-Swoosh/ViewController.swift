1da78fba90cbaf28887eaca552614198ad362f8c
Don Gruszka <dongruszka@Dons-MacBook-Pro.local>
2019-02-18 08:22:18 -0700

    frame modification

--- App-Swoosh/App-Swoosh/ViewController.swift
+++ App-Swoosh/App-Swoosh/ViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+     @IBOutlet weak var Swoosh: UIImageView!
+     
+     @IBOutlet weak var bgimg: UIImageView!
+     
+     
      override func viewDidLoad() {
           super.viewDidLoad()
+          
+          Swoosh.frame = CGRect(x: view.frame.size.width / 2 - Swoosh.frame.size.width / 2, y: 50, width: Swoosh.frame.size.width, height: Swoosh.frame.size.height)
+          
+          bgimg.frame = view.frame
+          
           // Do any additional setup after loading the view, typically from a nib.
      }
 
